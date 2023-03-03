139c412b1ccfc135bc8d72e2fbae145f2ea7a466
Priyabrato <priyabratonandi@gmail.com>
2023-03-03 13:08:20 +0530

    first message

---
+++ oop.py
@@ -0,0 +1,39 @@
+#Object Oriented Programming
+# class A:
+#     print("hello")
+# a = A()
+# class A:
+#     def method(self):
+#         print("hello")
+# a = A()
+# a.method()
+# A().method()
+# class pen:
+#    def type(self):
+#        print("gel")
+#    def color(self):
+#         print("red pen")    
+# a = pen() 
+# a.type()
+# pen().color()
+# b = pen()
+# print(type(b))
+# b.type()
+# c = pen()
+# pen().color()
+# c.type()
+# pen().type()
+class Rubel():
+    def hair(self):
+        print("brown")
+    def skin(self) :
+        print("white")
+class priyo():
+    def hair(self):
+        print("black")
+    def skin(self):
+        print("black")
+a = priyo()
+b = Rubel()
+a . hair()            
+b. skin()
